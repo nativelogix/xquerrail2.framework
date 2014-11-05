9031e364ca46c96a64397000c0ee1705b31f8896
Richard Louapre <richard.louapre@marklogic.com>
2014-11-05 06:07:24 -0500

    parseXml -> parseResponse

--- src/test/mocha/test/json-crud-test.js
+++ src/test/mocha/test/json-crud-test.js
@@ -22,7 +22,7 @@ function httpGet(model, action, data, callback) {
     followRedirect: true
   };
   request(options, function(error, response) {
-    return parseXml(model, error, response, callback);
+    return parseResponse(model, error, response, callback);
   });
 };
 
@@ -34,11 +34,11 @@ function httpPost(model, action, data, callback) {
     followRedirect: true
   };
   request(options, function(error, response) {
-    return parseXml(model, error, response, callback);
+    return parseResponse(model, error, response, callback);
   });
 };
 
-function parseXml(model, error, response, callback) {
+function parseResponse(model, error, response, callback) {
   if (response.statusCode === 500) {
     error = parseError(response);
   }

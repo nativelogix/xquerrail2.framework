27f6f1f2a97c60716c070c8520b94110e01383d7
Richard Louapre <richard.louapre@marklogic.com>
2015-06-11 14:33:03 -0400

    Fix cascade-delete-app-test

--- src/test/mocha/test/cascade-delete-app-test.js
+++ src/test/mocha/test/cascade-delete-app-test.js
@@ -39,31 +39,6 @@ var children = [
   }
 ];
 
-var searchFetch = function (model, data, callback) {
-  xquerrailCommon.model.search(model, data, function(error, response, entity) {
-    expect(response.statusCode).to.equal(200);
-    var total = entity.response.total;
-    var pageSize = entity.response['page_length'];
-    var items = [];
-    _.each(entity.response.results, function(item, index) {
-      xquerrailCommon.model.get(model, {'uri': item.uri}, function(error, response, entity) {
-        expect(response.statusCode).to.equal(200);
-        items.push(entity);
-        if (Math.min(total, pageSize) === index + 1) {
-          callback(items);
-        }
-      });
-    });
-  });
-};
-
-var getItem = function(model, name, callback) {
-  xquerrailCommon.model.get(model, {'name': name}, function(error, response, entity) {
-    expect(response.statusCode).to.equal(200);
-    expect(entity.name).to.equal(name);
-    callback(entity);
-  });
-};
 var deleteAll = function(callback) {
   xquerrailCommon.model.removeAll('parent-model', function(error, response) {
     expect(response.statusCode).to.equal(204);
@@ -75,32 +50,13 @@ var deleteAll = function(callback) {
 };
 
 var createAll = function(model, items, callback) {
-
-  var create = function(model, items, index, cb) {
-    if (items.length === index + 1) {
-      cb();
-    } else {
-      xquerrailCommon.model.create(model, items[index], function(error, response, entity) {
-        expect(response.statusCode).to.equal(200);
-        create(model, items, index+1, cb);
-      });
-    }
-  };
-
-  create(model, items, 0, callback);
-
-
-
-  // _.each(items, function(item, index) {
-  //   xquerrailCommon.model.create(model, item, function(error, response, entity) {
-  // //     expect(response.statusCode).to.equal(200);
-  // //     getItem(model, item.name, function() {
-  //       if (items.length === index + 1) {
-  //         callback();
-  //       }
-  // //     })
-  //   });
-  // });
+  _.each(items, function(item, index) {
+    xquerrailCommon.model.create(model, item, function(error, response, entity) {
+      if (items.length === index + 1) {
+        callback();
+      }
+    });
+  });
 };
 
 describe('Cascade delete features', function() {
@@ -109,20 +65,22 @@ describe('Cascade delete features', function() {
     xquerrailCommon.initialize(function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       xquerrailCommon.login(function(error, response, body) {
-        done();
+        deleteAll(function() {
+          done();
+        });
       });
     }, module.filename);
   });
 
   describe('cascade', function() {
     beforeEach(function(done) {
-      deleteAll(function() {
+      // deleteAll(function() {
         createAll('parent-model', parents, function() {
           createAll('child-model', children, function() {
             done();
           });
         });
-      });
+      // });
     });
 
     afterEach(function(done) {
@@ -144,43 +102,53 @@ describe('Cascade delete features', function() {
         _.each(referencedChildren, function (child, index) {
           xquerrailCommon.model.get('child-model', {'name': child.name}, function(error, response, entity) {
             expect(response.statusCode).to.equal(404);
+            if (referencedChildren.length === index + 1) {
+              done();
+            }
           });
         });
-        done();
       });
     });
 
     it('detach - should delete parent and referenced child', function(done) {
       var parentName = parents[1].name;
-      searchFetch('child-model', {'parent.text': parentName}, function(referencedChildren) {
-        xquerrailCommon.model.remove('parent-model', {'_cascade': 'detach', 'name': parentName}, function(error, response, entity) {
-          expect(response.statusCode).to.equal(200);
-          expect(entity.name).to.equal(parentName);
-          _.each(referencedChildren, function (child, index) {
-            xquerrailCommon.model.get('child-model', {'name': child.name}, function(error, response, entity) {
-              expect(response.statusCode).to.equal(200);
-              _.each(child.parent, function(parent) {
-                expect(parent.text).not.to.equal(parentName);
-              });
+      var referencedChildren = _.filter(
+        children,
+        function(child) { return (child.parent === parentName || child.parent.indexOf(parentName) > -1); }
+      );
+      xquerrailCommon.model.remove('parent-model', {'_cascade': 'detach', 'name': parentName}, function(error, response, entity) {
+        expect(response.statusCode).to.equal(200);
+        expect(entity.name).to.equal(parentName);
+        _.each(referencedChildren, function (child, index) {
+          xquerrailCommon.model.get('child-model', {'name': child.name}, function(error, response, entity) {
+            expect(response.statusCode).to.equal(200);
+            _.each(child.parent, function(parent) {
+              expect(parent.text).not.to.equal(parentName);
             });
+            if (referencedChildren.length === index + 1) {
+              done();
+            }
           });
-          done();
         });
       });
     });
 
-    xit('no cascade - should not delete parent', function(done) {
+    it('no cascade - should not delete parent', function(done) {
       var parentName = parents[2].name;
-      searchFetch('child-model', {'parent.text': parentName}, function(referencedChildren) {
-        xquerrailCommon.model.remove('parent-model', {'name': parentName}, function(error, response, entity) {
-          expect(response.statusCode).to.equal(500);
-          expect(error.name).to.equal('REFERENCE-CONSTRAINT-ERROR');
-          _.each(referencedChildren, function (child, index) {
-            xquerrailCommon.model.get('child-model', {'name': child.name}, function(error, response, entity) {
-              expect(response.statusCode).to.equal(200);
-            });
+      var referencedChildren = _.filter(
+        children,
+        function(child) { return (child.parent === parentName || child.parent.indexOf(parentName) > -1); }
+      );
+      xquerrailCommon.model.remove('parent-model', {'name': parentName}, function(error, response, entity) {
+        expect(response.statusCode).to.equal(500);
+        expect(error.name).to.equal('REFERENCE-CONSTRAINT-ERROR');
+        _.each(referencedChildren, function (child, index) {
+          xquerrailCommon.model.get('child-model', {'name': child.name}, function(error, response, entity) {
+            expect(response.statusCode).to.equal(200);
           });
-          done();
+          if (referencedChildren.length === index + 1) {
+            done();
+          }
         });
       });
     });
--- src/test/mocha/test/controller-extension-test.js
+++ src/test/mocha/test/controller-extension-test.js
@@ -4,98 +4,9 @@ var xquerrailCommon = require('./xquerrailCommon');
 var _ = require('lodash');
 var assert = require('chai').assert;
 var expect = require('chai').expect;
-var request = require('request').defaults({jar: true});
-var xml2js = require('xml2js');
-
-var parser = new xml2js.Parser({explicitArray: false});
-
-// function httpGet(model, action, data, format, callback) {
-//   var format = format || 'xml';
-//   var options = {
-//     json: true,
-//     method: 'GET',
-//     url: xquerrailCommon.urlBase + '/' + model + '/' + action + '.' + format,
-//     qs: data,
-//     followRedirect: true
-//   };
-//   request(options, function(error, response) {
-//     return parseResponse(model, error, response, format, callback);
-//   });
-// };
-
-// function httpPost(model, action, data, format, callback) {
-//   var format = format || 'xml';
-//   var options = {
-//     method: 'POST',
-//     url: xquerrailCommon.urlBase + '/' + model + '/' + action + '.' + format,
-//     form: data,
-//     followRedirect: true
-//   };
-//   request(options, function(error, response) {
-//     return parseResponse(model, error, response, format, callback);
-//   });
-// };
-
-// function parseResponse(model, error, response, format, callback) {
-//   if (format === 'json') {
-//     if (response.statusCode === 500) {
-//       error = parseError(response);
-//     }
-//     var entity = response.body;
-//     callback(error, response, entity);
-//   } else {
-//     if (response.body !== undefined) {
-//       var entity = response.body;
-//       parser.parseString(entity, function (err, result) {
-//         if (err !== null) {
-//           callback(err, response, undefined);
-//         } else {
-//           entity = (result !== null && result !== undefined)?result[model]: undefined;
-//           callback(error, response, entity);
-//         }
-//       });
-//     } else {
-//       callback(error, response, undefined);
-//     }
-//   }
-// };
-
-// function parseError(response) {
-//   return {
-//     code: response.body.error.code,
-//     message: response.body.error.message,
-//     description: response.body.error['format_string'],
-//     data: response.body.error.data,
-//     stack: response.body.error.stack
-//   }
-// };
-
-function create(model, data, callback) {
-  xquerrailCommon.httpMethod('POST', model, 'create', data, undefined, callback);
-};
-
-function update(model, data, callback) {
-  xquerrailCommon.httpMethod('POST', model, 'update', data, undefined, callback);
-};
-
-function get(model, data, format, callback) {
-  // httpGet(model, 'get', data, format, callback);
-  xquerrailCommon.httpMethod('GET', model, 'get', undefined, data, callback, format);
-};
-
-function edit(model, data, format, callback) {
-  // httpGet(model, 'edit', data, format, callback);
-  xquerrailCommon.httpMethod('GET', model, 'edit', undefined, data, callback, format);
-};
-
-function remove(model, data, callback) {
-  // httpPost(model, 'delete', data, 'xml', callback);
-  xquerrailCommon.httpMethod('POST', model, 'delete', data, undefined, callback);
-};
 
 describe('Controller extension features', function() {
 
-  // this.timeout(10000);
   before(function(done) {
     xquerrailCommon.initialize(function(error, response, body) {
       expect(response.statusCode).to.equal(200);

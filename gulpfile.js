3ac3f14585ae09218bc28c48eb2eda944f6584ea
Richard Louapre <richard.louapre@marklogic.com>
2015-06-30 09:00:06 -0400

    Update build script to better support Roxy deployer

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,10 @@ var plumber = require('gulp-plumber');
 var runSequence = require('run-sequence');
 var inject = require('gulp-inject-string');
 var nconf = require('nconf');
+var run = require('gulp-run');
+var map = require('vinyl-map');
+var exec = require('gulp-exec');
+var data = require('gulp-data');
 
 nconf
   .argv()
@@ -23,10 +27,66 @@ nconf
   .file({ file: './ml.json' });
 
 var ml = nconf.get('ml');
+var roxy = nconf.get('roxy');
+
+var getRoxyProperties = function() {
+  var options = {
+    'cwd': roxy.path
+  }
+  var exec = require('child_process').execSync;
+  var stdout = exec('ml ' + roxy.env + ' info --format=json', options)
+  return JSON.parse(stdout).properties;
+};
+
+var roxySettings = function() {
+  return {
+    'properties': getRoxyProperties()
+  };
+};
+
+var validateMlSettings = function () {
+  if (!ml) {
+    throw new gutil.PluginError('xquerrail', 'ml settings are required. They can be defined as arguments, environment variables, ml.json or from Roxy properties.')
+  }
+  if (!ml.port) {
+   throw new gutil.PluginError('xquerrail', 'ml.port is required.')
+  }
+  if (!ml.host) {
+   throw new gutil.PluginError('xquerrail', 'ml.host is required.')
+  }
+  if (!ml.user) {
+   throw new gutil.PluginError('xquerrail', 'ml.user is required.')
+  }
+  if (!ml.password) {
+   throw new gutil.PluginError('xquerrail', 'ml.password is required.')
+  }
+};
+
+if (!roxy) {
+  roxy = {};
+} else {
+  if (!ml) {
+    ml = {};
+  }
+  var properties = roxySettings().properties;
+  for (var key in properties) {
+    if (key.indexOf('ml.') == 0) {
+      if (!ml[key.substring(3)]) {
+        ml[key.substring(3)] = properties[key];
+      }
+    }
+  };
+  if (!ml.port) {
+    ml.port = ml['app-port'];
+  }
+  if (!ml.host) {
+    ml.host = ml['server'];
+  }
+}
+
 var version = pkg.version;
 var lastCommit;
 
-module.exports.ml = ml;
 gulp.task('update-xqy', ['copy-no-xqy', 'copy-xqy', 'last-git-commit'], function () {
   gutil.log('version: ' + version + ' - lastcommit: ' + lastCommit);
   return gulp.src(['dist/**/config.xqy'])
@@ -55,6 +115,41 @@ gulp.task('last-git-commit', function(cb) {
     }))
 })
 
+gulp.task('roxy:watch', function() {
+  var modulesDB = roxySettings().properties['ml.modules-db'];
+  var environment = roxy.env;
+  var path = require('path');
+  var options = {
+    'cwd': 'roxy',
+    pipeStdout: true,
+    roxy : {
+      'modulesDB': modulesDB,
+      'environment': environment
+    },
+    'normalize': function(s) {
+      s = s.replace(/\\/g,'/');
+      if (s.substring(s.length-1) === '/') {
+        s = s.substring(0, s.length-1)
+      }
+      return s;
+    },
+    'join': function(f) {
+      return path.join(f.cwd, f.base);
+    }
+  };
+  var reportOptions = {
+    err: true, // default = true, false means don't write err
+    stderr: true, // default = true, false means don't write stderr
+    stdout: true // default = true, false means don't write stdout
+  }
+
+  watch(['src/**/*.xqy'], {read: false})
+    .pipe(plumber())
+    .pipe(exec('echo <%= options.normalize(file.path) %>', options))
+    // .pipe(exec('ml <%= options.roxy.environment %> load <%= options.normalize(file.path) %> --db=<%= options.roxy.modulesDB %> --remove-prefix=<%= options.normalize(options.join(file)) %> -v', options))
+    .pipe(exec.reporter(reportOptions));
+});
+
 gulp.task('coverage', function () {
 });
 
@@ -62,10 +157,7 @@ gulp.task('lint', function () {
 });
 
 gulp.task('xray', function (cb) {
-  if (!ml) {
-    throw new gutil.PluginError('xquerrail', 'ml settings are required. They can be defined as arguments, environment variables or json file.')
-  }
-
+  validateMlSettings();
   var options = {
     /* https://github.com/mikeal/request#http-authentication */
     auth: {
@@ -81,6 +173,7 @@ gulp.task('xray', function (cb) {
 });
 
 gulp.task('mocha', function (cb) {
+  validateMlSettings();
   var mochaOptions = {
     timeout: 15000,
     reporter: 'spec'
@@ -144,3 +237,5 @@ gulp.task('default', function() {
         console.log('Build completed.');
     });
 });
+
+module.exports.ml = ml;
